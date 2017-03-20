7a6801393170326619865592562ca53fa27f45bb
陈冬春 <Ecdc1992@163.com>
2017-03-20 16:57:47 +0800

    Add toHexCommandLine

--- DeviceTool/MKSerialController.m
+++ DeviceTool/MKSerialController.m
@@ -26,6 +26,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark 生成16进制命令
+- (NSData *)toHexCommandline:(char *)commandLine{
+    unsigned char data[5]= {};
+    
+    int firstNum = (int)strtol(&commandLine[0], NULL, 16);
+    int secondNum = (int)strtol(&commandLine[1], NULL, 16);
+    int thirdNum = (int)strtol(&commandLine[2], NULL, 16);
+    int forthNum = (int)strtol(&commandLine[3], NULL, 16);
+    int fifthNum = (int)strtol(&commandLine[4], NULL, 16);
+    
+    printf("%X-%X-%X-%X-%X",firstNum,secondNum,thirdNum,forthNum,fifthNum);
+    data[0] = (char)firstNum;
+    data[1] = (char)secondNum;
+    data[2] = (char)thirdNum;
+    data[3] = (char)forthNum;
+    data[4] = (char)fifthNum;
+    
+    NSData *dataB =[NSData dataWithBytes:data length:5];
+    return dataB;
+}
+
 /*
 #pragma mark - Navigation
 
