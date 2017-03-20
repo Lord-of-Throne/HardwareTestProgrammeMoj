2334b39cce34fbd4f5f245f6ce73ebefaa7f8ee1
陈冬春 <Ecdc1992@163.com>
2017-03-20 16:47:37 +0800

    Add bluetooth class

---
+++ DeviceTool/MojoyBluetoothMgr.h
@@ -0,0 +1,13 @@
+//
+//  MojoyBluetoothMgr.h
+//  DeviceTool
+//
+//  Created by xxl on 2017/3/20.
+//  Copyright © 2017年 Monky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MojoyBluetoothMgr : NSObject
+
+@end
