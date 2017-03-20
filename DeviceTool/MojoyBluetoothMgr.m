d63316a45ea04565cfad9f2b4bc4361dbcf2179c
Monky <monky_q@163.com>
2017-03-20 16:56:36 +0800

    add BlueTooth single init

--- DeviceTool/MojoyBluetoothMgr.m
+++ DeviceTool/MojoyBluetoothMgr.m
@@ -8,6 +8,39 @@
 
 #import "MojoyBluetoothMgr.h"
 
+@interface MojoyBluetoothMgr()<CBCentralManagerDelegate,CBPeripheralDelegate>
+
+@property(nonatomic,strong)CBCentralManager* centralManager;
+@property(nonatomic,strong)NSMutableArray* dicoveredPeripherals;
+
+@end
+
+
 @implementation MojoyBluetoothMgr
+#pragma mark - Init method
+
++ (instancetype)shareBlueTooth{
+    static dispatch_once_t once;
+    static MojoyBluetoothMgr* blueTooth;
+    dispatch_once(&once, ^{
+        blueTooth = [[self alloc] init];
+    });
+    return blueTooth;
+}
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:NO], CBCentralManagerOptionShowPowerAlertKey, nil];
+        
+        self.dicoveredPeripherals = [NSMutableArray new];
+        
+        CBCentralManager *central = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:options];
+        self.centralManager = central;
+
+        
+    }
+    return self;
+}
 
 @end
